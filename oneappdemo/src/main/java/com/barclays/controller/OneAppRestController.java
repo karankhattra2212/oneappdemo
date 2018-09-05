d3ee854772aa1f5172958a4b172c20061317aeba
AzureAD\ParamveerSingh <karankhattra2212@gmail.com>
2018-09-05 17:24:28 +0530

    Project Structure

---
+++ oneappdemo/src/main/java/com/barclays/controller/OneAppRestController.java
@@ -0,0 +1,19 @@
+package com.barclays.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class OneAppRestController {
+
+    @PostMapping
+    public void saveTermsAndCond( ){
+        //To do Vatsala
+    }
+
+    @GetMapping
+    public void getTermsAndCond( ){
+        //To do Vatsala
+    }
+
+
+}
