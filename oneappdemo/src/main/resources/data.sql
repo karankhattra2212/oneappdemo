02c77e8038f8bade4a171c06020939dfab294d35
Deepika Jain <deepikaj@in.ibm.com>
2018-09-11 11:43:23 +0530

    CustomerDTO updated, H2 database creation  code, build file modified with db dependencies

---
+++ oneappdemo/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into CUSTOMER
+values(10001,'MS', 'abs','abs','abs','abs','2004-12-31','abs','','123456','y','Active');
+
+insert into ADDRESS
+      values(10002,'10001', 'abs','abs','abs','abs','A23456');
+
+ insert into DEVICE
+               values(10003,'10001', '2','2004-12-31 11:11:11');
---
+++ oneappdemo/src/main/resources/schema.sql
@@ -0,0 +1,46 @@
+
+
+CREATE TABLE CUSTOMER (
+   CUSTOMERID integer not null,
+	TITLE varchar(255) not null,
+	FIRSTNAME varchar(255),
+	MIDDLENAME varchar(50),
+	LASTNAME varchar(50),
+	PREVIOUSNAME varchar(50),
+	DATEOFBIRTH DATE,
+	NATIONALITY varchar(50),
+	ADDRESSID varchar(50),
+	PASSCODE int,
+	TNC CHAR(1),
+	STATE varchar(10),
+	primary key(CUSTOMERID)
+   );
+
+
+
+
+   CREATE TABLE ADDRESS (
+    ADDRESSID integer not null,
+   	CUSTOMERID integer not null ,
+   	FROMDATE varchar(50),
+   	TODATE varchar(50),
+   	COUNTRY varchar(20),
+   	ADDRESS1 varchar(100),
+   	POSTCODE varchar(8),
+   	foreign key(CUSTOMERID)
+   	REFERENCES customer(CUSTOMERID)
+      );
+
+
+
+      CREATE TABLE DEVICE (
+         DEVICEID integer not null,
+      	CUSTOMERID integer,
+      	APPVERSION integer,
+      	LASTMODIFIED TIMESTAMP(6)
+         );
+
+
+
+
+
